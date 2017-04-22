fd57f0c63e96ca86a252f314d8fbe06cad133a59
Renderay <polupan.andrew@gmail.com>
2017-04-22 14:36:45 +0300

    1

---
+++ css/Java.js
@@ -0,0 +1,17 @@
+var container = document.getElementsByClassName("wrapper")[0];
+var moveRight = function () {
+container.style.margin="5px";
+}
+
+container.addEventListener("mouseover", moveRight);
+
+
+var moveRight = function () {
+container.style.margin="0 170 180 0";
+}
+
+
+
+
+
+container.addEventListener("mouseout", moveRight);
